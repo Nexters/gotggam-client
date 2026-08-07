f415325d37b8dcc9fa175e08de17ec08ddfa8e9b
ppinppini <bnm0218@naver.com>
2026-08-07 21:49:29 +0900

    feat: 아이콘 버튼 컴포넌트 추가

---
+++ src/shared/ui/icon-button/icon-button.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+export const iconButton = style({
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: 0,
+  border: "none",
+  background: "none",
+  cursor: "pointer",
+});
--- src/shared/ui/icon/icon-button-arrow.tsx
+++ src/shared/ui/icon/icon-button-arrow.tsx
@@ -1,5 +1,7 @@
 import type { SVGProps } from "react";
 
+import { cn } from "@/shared/lib";
+
 import ButtonArrowSvg from "./assets/button-arrow.svg";
 import * as styles from "./icon.css";
 
@@ -7,11 +9,7 @@ type IconProps = SVGProps<SVGSVGElement>;
 
 // button-arrow.svg 원본이 왼쪽 방향
 function ArrowRight({ className, ...props }: IconProps) {
-  const mergedClassName = className
-    ? `${styles.arrowRight} ${className}`
-    : styles.arrowRight;
-
-  return <ButtonArrowSvg className={mergedClassName} {...props} />;
+  return <ButtonArrowSvg className={cn(styles.arrowRight, className)} {...props} />;
 }
 
 export const IconButtonArrow = {

26075991a82691a52cc2106fe2c4cdf5e70b727d
ppinppini <bnm0218@naver.com>
2026-08-15 12:55:39 +0900

    fix: 이름 유효성 표시에 디바운스 적용

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -2,6 +2,7 @@ export { playAnimalese } from "./animalese";
 export { default as cn } from "clsx";
 export { delay } from "./delay";
 export { playSfx, setAudioPlaying, useAudio } from "./use-audio";
+export { useDebouncedValue } from "./use-debounced-value";
 export { useHasMounted } from "./use-has-mounted";
 export { useLocalStorage } from "./use-local-storage";
 export { useTypewriter } from "./use-typewriter";
